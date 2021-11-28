f55d3968a014365881b4a3dcb778f46d2336931f
romiras-coder <rom_iras@mail.ru>
2021-11-28 23:24:15 +0300

    сделал проверки

--- FastAPI_project/main.py
+++ FastAPI_project/main.py
@@ -1,8 +1,8 @@
 import datetime
-
+import numpy as np
 import uvicorn
 from fastapi import FastAPI, Query, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Field
 from typing import Optional
 from datetime import datetime, time, timedelta
 
@@ -15,13 +15,35 @@ class Item(BaseModel):
     amount: int
     rate: float
 
+    @validator('periods')
+    def periods_must_be_in_interval(cls, v):
+        if v not in range(1, 61):
+            raise HTTPException(status_code=400,
+                                detail={"error": f"период (periods) должен быть не меньше 1 и не больше 60"})
+        return v
+
+    @validator('amount')
+    def amount_must_be_in_interval(cls, v):
+        if v not in range(10000, 3000001):
+            raise HTTPException(status_code=400,
+                                detail={
+                                    "error": f"сумма вклада (amount) должна быть не меньше 10000 и не превышать 3000000"})
+        return v
+
+    @validator('rate')
+    def rate_must_be_in_interval(cls, v):
+        if v not in np.arange(1.0, 8.1, 0.1).round(1):
+            raise HTTPException(status_code=400,
+                                detail={"error": f"процентная ставка (rate) должна быть не меньше 1 и не превышать 8"})
+        return v
+
     class Config:
         schema_extra = {
             "example": {
                 "date": "31.01.2021",
                 "periods": 7,
                 "amount": 10000,
-                "rate": 8.1,
+                "rate": 8,
             }
         }
 
@@ -34,15 +56,15 @@ async def root():
 # создание нового пользователя, метод POST, поля формы, возврат JSON
 @app.post("/test/")
 async def create_item(item: Item):
-    item_dict = item.dict()
-    print(item_dict)
-    print(item_dict['date'], type(item_dict['date']))
-    print(item_dict['periods'], type(item_dict['periods']))
-    print(item_dict['amount'], type(item_dict['amount']))
-    print(item_dict['rate'], type(item_dict['rate']))
+    # item_dict = item.dict()
+    # print(item_dict)
+    # print(item_dict['date'], type(item_dict['date']))
+    # print(item_dict['periods'], type(item_dict['periods']))
+    # print(item_dict['amount'], type(item_dict['amount']))
+    # print(item_dict['rate'], type(item_dict['rate']))
 
-    if item_dict['periods'] not in range(1, 60):
-        raise HTTPException(status_code=400, detail={"error": f"период не может быть отрицательным"})
+    # if item_dict['periods'] not in range(1, 60):
+    #     raise HTTPException(status_code=400, detail={"error": f"период не может быть отрицательным"})
     # elif item_dict['periods'] > 60:
     #     raise HTTPException(status_code=400, detail={"error": f"период не может быть больше 60 месяцев"})
     # elif item_dict['amount'] > 60:
@@ -55,7 +77,7 @@ async def create_item(item: Item):
     # if item.tax:
     #     price_with_tax = item.price + item.tax
     #     item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+    return item
 
 
 # main
